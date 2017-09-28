1c538256b2ac9442fe501005527b1b55ba8016b7
JannikHv <JannikHv@gmail.com>
2017-09-28 18:30:48 +0200

    Initial release

---
+++ README.md
@@ -0,0 +1,2 @@
+# Die FIRMA
+This is the official website of the firm Die FIRMA.
---
+++ js/main.js
@@ -0,0 +1,38 @@
+/* Globals */
+var last_target = $('#about-page');
+
+$(document).ready(function() {
+    $('.main-view').fadeIn(2000);
+    $('header').hide().slideDown(1250);
+    $('header #logo img').hide().fadeIn(2000);
+
+    /* Check if window is too small to show sidebars */
+    if ($(window).width() > 1320)
+        $('.main-view .sbar').hide().delay(750).fadeIn(1750);
+
+    $(window).on('resize', function() {
+        if ($(this).width() <= 1320)
+            $('.main-view .sbar').fadeOut(300);
+        else
+            $('.main-view .sbar').fadeIn(300);
+    });
+
+    $('header #logo img').on('click', function(event) {
+        location.reload();
+    })
+
+    $('a').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+
+        if (!target.is(last_target)) {
+            last_target.hide();
+            target.fadeIn(1000);
+        }
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: '0px'
+        }, 1000);
+
+        last_target = target;
+    });
+});
